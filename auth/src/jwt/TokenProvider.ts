dc23568a45fadd64648e203c785ef40c60a22973
nuri35 <nurie487@gmail.com>
2023-08-01 20:11:24 +0300

    Add general token

---
+++ auth/src/jwt/TokenProvider.ts
@@ -0,0 +1,22 @@
+import jwt from 'jsonwebtoken';
+
+export class TokenProvider {
+  constructor() {}
+
+  static signJWT(id: number): string {
+    const token = jwt.sign({ id: id }, process.env.JWT_KEY!, {
+      expiresIn: process.env.JWT_EXPIRES_IN,
+    });
+    return token;
+  }
+
+  static generateUrlToken(id: number): string {
+    const token = TokenProvider.signJWT(id);
+    return ` 
+    http://
+    ${process.env.AUTH_HOST}
+    /api/auth/wVerify
+    ?token=${token}
+    `;
+  }
+}
