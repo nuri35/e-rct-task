80db079f6a2786ce7f04beb54a9ec84b5cb43f2f
nuri35 <nurie487@gmail.com>
2023-08-01 15:28:09 +0300

    Set up user entity for db process

---
+++ auth/src/entities/User.ts
@@ -0,0 +1,52 @@
+import { IsNotEmpty, Length, IsEmail } from 'class-validator';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  Unique,
+  UpdateDateColumn,
+  BeforeInsert,
+} from 'typeorm';
+import { BaseEntity } from './Base';
+import { Exclude, Transform } from 'class-transformer';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  public id!: number;
+
+  @Column()
+  @Length(4, 20)
+  @IsNotEmpty()
+  public name!: string;
+
+  @Column()
+  @Length(4, 20)
+  @IsNotEmpty()
+  public lastname!: string;
+
+  @Column({ select: false })
+  @Length(4, 100)
+  @IsNotEmpty()
+  public password!: string;
+
+  static removePassword(userObj: User) {
+    return Object.fromEntries(
+      Object.entries(userObj).filter(([key, val]) => key !== 'password')
+    );
+  }
+
+  @Column()
+  @Length(4, 20)
+  @IsEmail()
+  public email!: string;
+
+  @Column({
+    default: false,
+  })
+  public isActive!: boolean;
+}
+
+export default User;
