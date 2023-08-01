91b2c73eb5b67a0c5884b1a4cae919b9ffb9590e
nuri35 <nurie487@gmail.com>
2023-08-01 15:28:37 +0300

    Request validation control

---
+++ auth/src/schema/signupValidation.ts
@@ -0,0 +1,20 @@
+import { body } from 'express-validator';
+import { Password } from '../passwordService/password';
+
+const validationSignup = {
+  signupSchema: [
+    body('email').isEmail().withMessage('Email must be valid'),
+    body('password').custom((value: string, { req }) => {
+      const schema = Password.schemaValidator();
+      const isPasswordValid = schema.validate(value) as boolean;
+      if (!isPasswordValid) {
+        throw new Error('password not valid');
+      }
+      return true;
+    }),
+    body('name').not().isEmpty().withMessage('Name is required'),
+    body('lastname').not().isEmpty().withMessage('Lastname is required'),
+  ],
+};
+
+export { validationSignup };
