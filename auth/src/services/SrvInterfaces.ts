d63037268c6fdbc05473e5fe4438e375b96be1fb
nuri35 <nurie487@gmail.com>
2023-08-02 22:30:55 +0300

    Add interface input data

---
+++ auth/src/services/SrvInterfaces.ts
@@ -0,0 +1,11 @@
+export interface SigninInput {
+  email: string;
+  password: string;
+}
+
+export interface SignupInput {
+  email: string;
+  password: string;
+  name: string;
+  lastName: string;
+}
