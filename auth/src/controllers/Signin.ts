2449284b1d744795aed1c9c171ce2e1493b5d759
nuri35 <nurie487@gmail.com>
2023-08-02 22:31:40 +0300

    Updated

--- auth/src/controllers/Signin.ts
+++ auth/src/controllers/Signin.ts
@@ -1,11 +1,20 @@
 import { Request, Response } from 'express';
 import { StatusCodes, ReasonPhrases } from 'http-status-codes';
 import { SigninSrv } from '../services/Signin';
+import { SigninInput } from '../services/SrvInterfaces';
 
 class SigninController {
   async signinHandler(req: Request, res: Response) {
     try {
-      return 'ok'; // en son duzenlenecek
+      const valueObj = await SigninSrv.signin(req.body as SigninInput);
+      // // Store it on session object
+      req.session = {
+        jwt: valueObj.token,
+      };
+      return res.status(StatusCodes.OK).send({
+        message: ReasonPhrases.OK,
+        data: valueObj.user,
+      });
     } catch (error) {
       throw error;
     }
--- auth/src/services/Signin.ts
+++ auth/src/services/Signin.ts
@@ -2,16 +2,38 @@ import { User } from '../entities/User';
 import { DataSource } from 'typeorm';
 import { BadRequestError, EmailType } from '@fbticketss/common';
 import { databaseSource } from '../database/dataSource';
+import { SigninInput } from './SrvInterfaces';
+import { Password } from '../passwordService/password';
+import { TokenProvider } from '../jwt/TokenProvider';
 
 class SigninService {
   private cn!: DataSource;
   constructor() {
     this.cn = databaseSource._connection;
   }
-  async signin(user: User) {
+  async signin(data: SigninInput) {
     try {
       const userRepo = this.cn.getRepository(User);
-      return 'ok';
+      const existingUser = await userRepo.findOne({
+        where: { email: data.email, isActive: true },
+      });
+
+      if (!existingUser) {
+        throw new BadRequestError('Invalid credentials');
+      }
+
+      const passwordsMatch = await Password.compare(
+        existingUser.password,
+        data.password
+      );
+      if (!passwordsMatch) {
+        throw new BadRequestError('Invalid Credentials');
+      }
+
+      const token = TokenProvider.signJWT(existingUser.id);
+      const { password, ...userWithoutPassword } = existingUser;
+
+      return { token, user: userWithoutPassword };
     } catch (error: any) {
       throw error;
     }
