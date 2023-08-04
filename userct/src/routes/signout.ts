d938523440d6906df662b83af46ea01f3913403c
nuri35 <nurie487@gmail.com>
2023-08-04 16:51:11 +0300

    feat: :art: Add signout route with user device clearing
    - Implemented a new route in the Express app to handle user signout.
     - This route clears the session and removes the deviceUser field from the User entity in the database.

--- userct/src/routes/signout.ts
+++ userct/src/routes/signout.ts
@@ -1,10 +1,20 @@
 import express from 'express';
 import { requireAuth } from '@fbticketss/common';
+import { databaseSource } from '../database/dataSource';
+import User from '../entities/User';
 
 const router = express.Router();
 
-router.post('/api/user/signout', requireAuth, (req, res) => {
+router.post('/api/user/signout', requireAuth, async (req, res) => {
   req.session = null;
+  const cn = databaseSource._connection;
+  const userRepo = cn.getRepository(User);
+  const existingUser = (await userRepo.findOne({
+    where: { id: req.currentUser!.id as number, isActive: true },
+  })) as User;
+
+  existingUser.deviceUser = null as unknown as string;
+  await userRepo.save(existingUser);
 
   res.send({});
 });
