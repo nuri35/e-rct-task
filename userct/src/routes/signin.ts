e2a2491ee3917432c6660237ce7209c9d50ea085
nuri35 <nurie487@gmail.com>
2023-08-04 16:52:25 +0300

    Add sessioncontrol for middleweare

--- userct/src/routes/signin.ts
+++ userct/src/routes/signin.ts
@@ -2,11 +2,13 @@ import express from 'express';
 import { validateRequest } from '@fbticketss/common';
 import { validationSignin } from '../schema/signinValidation';
 import { SigninCntrol } from '../controllers/Signin';
+import { sessionControl } from '../middleweare/sessionControl';
 
 const router = express.Router();
 
 router.post(
   '/api/user/signin',
+  sessionControl,
   validationSignin.signinSchema,
   validateRequest,
   SigninCntrol.signinHandler
